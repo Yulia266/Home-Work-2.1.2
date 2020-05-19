33f7c17290335fa14cc7968dd8c42406b22f8fe7
yulia266 <yulia266@mail.ru>
2020-05-19 20:51:40 +0300

    Initial Commit

---
+++ Home Work 2.1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home Work 2.1.2
+//
+//  Created by Юлия on 19.05.2020.
+//  Copyright © 2020 Юлия. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
